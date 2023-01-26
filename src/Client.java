ea36a380a4f8a9b0a83308c42ed061b8f219c98e
Olivier Falardeau <olivierfalardeau60@gmail.com>
2023-01-26 13:32:32 -0500

    download creats error in client

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,16 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 // Application client
 public class Client {
 	private static Socket socket;
+	private static Scanner scanner = new Scanner(System.in);
+	private static DataInputStream in = null;
+	private static DataOutputStream out = null;
 	public static void main(String[] args) throws Exception {
 		// Adresse et port du serveur
 		String serverAddress = "127.0.0.1";
@@ -13,29 +19,62 @@ public class Client {
 		socket = new Socket(serverAddress, port);
 		System.out.format("Serveur lancé sur [%s:%d]", serverAddress, port);
 		// Céatien d'un canal entrant pour recevoir les messages envoyés, par le serveur
-		DataInputStream in = new DataInputStream(socket.getInputStream());
+		in = new DataInputStream(socket.getInputStream());
 		// Attente de la réception d'un message envoyé par le, server sur le canal
 		String helloMessageFromServer = in.readUTF();
 		System.out.println(helloMessageFromServer);
 		// fermeture de La connexion avec le serveur
-		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-		Scanner scanner = new Scanner(System.in);
+		out = new DataOutputStream(socket.getOutputStream());
+		//Scanner scanner = new Scanner(System.in);
 		boolean exit = false;
 		while(!exit) {
 			String userInput = scanner.nextLine();
-			switch (userInput) {
+			String[] expressions = userInput.split(" ");
+			switch (expressions[0]) {
 			case "exit":
 				out.writeUTF(userInput);
 				exit = true;
 				break;
+			case "downlaod":
+				out.writeUTF(userInput);
+				receiveFile(expressions[1]);
+				break;
 			default:
 				out.writeUTF(userInput);
+				System.out.println(in.readUTF());
 				break;
 				
 			}
+
+		}
 		scanner.close();
 		socket.close();	
-		}
-
 	}
+	private static void sendFile(String path) throws Exception{
+        int bytes = 0;
+        File file = new File(path);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        
+        // send file size
+        out.writeLong(file.length());  
+        // break file into chunks
+        byte[] buffer = new byte[4*1024];
+        while ((bytes=fileInputStream.read(buffer))!=-1){
+            out.write(buffer,0,bytes);
+            out.flush();
+        }
+        fileInputStream.close();
+    }
+	private static void receiveFile(String fileName) throws Exception{
+        int bytes = 0;
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        
+        long size = in.readLong();     // read file size
+        byte[] buffer = new byte[4*1024];
+        while (size > 0 && (bytes = in.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1) {
+            fileOutputStream.write(buffer,0,bytes);
+            size -= bytes;      // read upto file size
+        }
+        fileOutputStream.close();
+    }
 }
--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,28 +1,46 @@
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
 public class ClientHandler extends Thread{
 	private Socket socket; 
-	private int clientNumber; 
+	private int clientNumber;
+	private static DataInputStream in = null;
+	private static DataOutputStream out = null;
 	public ClientHandler(Socket socket, int clientNumber) {
 		this.socket = socket;
 		this.clientNumber = clientNumber; 
 		System.out.println("New connection with client#" + clientNumber + " at" + socket);
 	}
-	public void run() { // Création de thread qui envoi un message à un client
+	public void run(){ // Création de thread qui envoi un message à un client
 		try {
-			DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // création de canal d’envoi 
+			out = new DataOutputStream(socket.getOutputStream()); // création de canal d’envoi 
 			out.writeUTF("Hello from server - you are client#" + clientNumber);// envoi de message
 			boolean exit = false;
-			DataInputStream in = new DataInputStream(socket.getInputStream());
+			in = new DataInputStream(socket.getInputStream());
 			while(!exit) {
 				String userInput = in.readUTF();
-				switch(userInput) {
+				String[] expressions = userInput.split(" ");
+				switch(expressions[0]) {
 				case "exit":
 					exit = true;
 					break;
+				case "hello":
+					System.out.println("hello from client");
+					out.writeUTF("hello from server");
+					break;
+				case "download":
+					try {sendFile(expressions[1]);}
+					catch(Exception e) {}
+					break;
+				default:
+					out.writeUTF("Enter an appropriate input");
+					break;
 				}
+				
 			}
 			
 		}catch (IOException e) {
@@ -36,4 +54,31 @@ public class ClientHandler extends Thread{
 			System.out.println("Connection with client# " + clientNumber+ " closed");
 		}
 	}
+	private static void sendFile(String path) throws Exception{
+        int bytes = 0;
+        File file = new File(path);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        
+        // send file size
+        out.writeLong(file.length());  
+        // break file into chunks
+        byte[] buffer = new byte[4*1024];
+        while ((bytes=fileInputStream.read(buffer))!=-1){
+            out.write(buffer,0,bytes);
+            out.flush();
+        }
+        fileInputStream.close();
+    }
+	private static void receiveFile(String fileName) throws Exception{
+        int bytes = 0;
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        
+        long size = in.readLong();     // read file size
+        byte[] buffer = new byte[4*1024];
+        while (size > 0 && (bytes = in.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1) {
+            fileOutputStream.write(buffer,0,bytes);
+            size -= bytes;      // read upto file size
+        }
+        fileOutputStream.close();
+    }
 }
